8632d019d46491ebe4ce1d222df20a38d6c2b2bf
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-02-15 23:40:01 -0500

    cleaned up code

--- app/src/main/java/com/fro/recylerewards/CameraActivity.java
+++ app/src/main/java/com/fro/recylerewards/CameraActivity.java
@@ -1,57 +1,35 @@
 package com.fro.recylerewards;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.video.VideoCapture;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.lifecycle.LifecycleOwner;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.File;
-import java.net.URI;
-import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
-import com.google.common.util.concurrent.ListenableFuture;
 
 public class CameraActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     PreviewView previewView;
-    private ImageCapture imageCapture;
-    private Button bCapture;
+    ImageCapture imageCapture;
+    Button bCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,18 +47,15 @@ public class CameraActivity extends AppCompatActivity {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                 startCameraX(cameraProvider);
             } catch (ExecutionException | InterruptedException e) {
-                String text = "Error: "+ e.getMessage();
-                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Error: "+ e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }, getExecutor());
-
     }
 
     Executor getExecutor() {
         return ContextCompat.getMainExecutor(this);
     }
 
-    @SuppressLint("RestrictedApi")
     private void startCameraX(ProcessCameraProvider cameraProvider) {
         cameraProvider.unbindAll();
         CameraSelector cameraSelector = new CameraSelector.Builder()
@@ -95,23 +70,9 @@ public class CameraActivity extends AppCompatActivity {
                 .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
                 .build();
 
-//        // Image analysis use case
-//        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
-//                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-//                .build();
-//
-//        imageAnalysis.setAnalyzer(getExecutor(), (ImageAnalysis.Analyzer) this);
-
         //bind to lifecycle:
         cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
     }
-
-    public void analyze(@NonNull ImageProxy image) {
-        // image processing here for the current frame
-        Log.d("TAG", "analyze: got the frame at: " + image.getImageInfo().getTimestamp());
-        image.close();
-    }
-
 
     private void capturePhoto() {
         long timestamp = System.currentTimeMillis();
@@ -125,8 +86,7 @@ public class CameraActivity extends AppCompatActivity {
                         getContentResolver(),
                         MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                         contentValues
-                ).build(),
-                getExecutor(),
+                ).build(), getExecutor(),
                 new ImageCapture.OnImageSavedCallback() {
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
@@ -139,6 +99,5 @@ public class CameraActivity extends AppCompatActivity {
                     }
                 }
         );
-
     }
 }
