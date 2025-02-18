477699704f3e0dddf2a754a9aa23ce347717765b
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-02-17 19:12:48 -0500

    Mock chat bot functionality

--- app/src/main/java/com/fro/sustainabilitysidequests/BotActivity.java
+++ app/src/main/java/com/fro/sustainabilitysidequests/BotActivity.java
@@ -1,8 +1,14 @@
 package com.fro.sustainabilitysidequests;
 
+import static android.view.View.VISIBLE;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +20,14 @@ public class BotActivity extends AppCompatActivity {
     // Declare global variables here \/
     Button home;
     Button shop;
+    Button enter;
+    View placeholder;
+    LinearLayout response;
+    LinearLayout yourLayout;
+    TextView yourText;
+    TextView completed;
+    EditText input;
+    boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +37,50 @@ public class BotActivity extends AppCompatActivity {
         // Instantiate variables here \/
         home = findViewById(R.id.home);
         shop = findViewById(R.id.shop);
+        enter = findViewById(R.id.enter);
+        placeholder = findViewById(R.id.placeholder);
+        response = findViewById(R.id.response);
+        yourLayout = findViewById(R.id.yourLayout);
+        yourText = findViewById(R.id.yourText);
+        completed = findViewById(R.id.completed);
+        input = findViewById(R.id.input);
 
         // Changing pages
         home.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), MainActivity.class)));
         shop.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), ShopActivity.class)));
+
+        // Setting completed to saved value
+        completed.setText(Values.completed);
+
+        // Mock chatbot
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!input.getText().toString().isEmpty() && !clicked){
+                    String text = input.getText().toString();
+                    yourText.setText(text);
+                    yourLayout.setVisibility(VISIBLE);
+                    response.setVisibility(VISIBLE);
+
+                    //sets the empty view to have a lower weight
+                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            0,
+                            1.0f
+                    );
+                    placeholder.setLayoutParams(param);
+
+                    clicked = true;
+                    input.setHint("Coming soon!");
+                    Values.completed = "(Weekly chat is completed)";
+                    text = Values.completed +" +100 Points";
+                    completed.setText(text);
+                    Values.points += 100;
+                }
+                else if (!clicked) {
+                    input.setHint("Please enter text");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/fro/sustainabilitysidequests/Values.java
+++ app/src/main/java/com/fro/sustainabilitysidequests/Values.java
@@ -5,4 +5,6 @@ import androidx.camera.core.ImageProxy;
 
 public class Values {
     public static ImageProxy image;
+    public static String completed = "(Weekly chat not completed yet)";
+    public static int points = 0;
 }
